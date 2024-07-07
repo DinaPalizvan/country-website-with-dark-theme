05cf566f6d2097d63412494aff1061cda35e9bfb
DinaPalizvan <d.palizvaan99@gmail.com>
2024-07-07 19:27:35 +0330

    final commit

--- Countries/src/pages/HomePage.jsx
+++ Countries/src/pages/HomePage.jsx
@@ -25,6 +25,16 @@ export default function HomePage() {
       setData(newData);
     });
   }, [searchData, selectedRegion]);
+  
+  data?.sort((a, b) => {
+    if (a.name.common < b.name.common ) {
+      return -1;
+    }
+    if (a.name.common  > b.name.common ) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
     <>
